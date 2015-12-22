913f985742cf67d495648519f52d79f006bf51c7
Vladislav Radchenko <v.radchenko@madnet-team.ru>
2015-12-22 19:55:20 +0300

    +

---
+++ game/src/main/java/com/nuclearthinking/game/obj/Player.java
@@ -0,0 +1,15 @@
+package com.nuclearthinking.game.obj;
+
+/**
+ * Date: 22.12.2015
+ * Time: 19:52
+ *
+ * @author Vladislav Radchenko (onifent@gmail.com)
+ */
+
+public class Player {
+    private int hitPoints;
+    private PlayerClass pClass;
+    private int level;
+    private String name;
+}
---
+++ game/src/main/java/com/nuclearthinking/game/obj/PlayerClass.java
@@ -0,0 +1,11 @@
+package com.nuclearthinking.game.obj;
+
+/**
+ * Date: 22.12.2015
+ * Time: 19:53
+ *
+ * @author Vladislav Radchenko (onifent@gmail.com)
+ */
+
+public class PlayerClass {
+}
