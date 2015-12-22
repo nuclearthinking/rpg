c87fd6e97f5ddcef330b51497beb04482ae1bfef
Vladislav Radchenko <v.radchenko@madnet-team.ru>
2015-12-22 19:43:44 +0300

    init commit

---
+++ game/src/main/java/com/nuclearthinking/game/utils/Printer.java
@@ -0,0 +1,11 @@
+package com.nuclearthinking.game.utils;
+
+/**
+ * Date: 22.12.2015
+ * Time: 19:36
+ *
+ * @author Vladislav Radchenko (onifent@gmail.com)
+ */
+
+public class Printer {
+}
---
+++ game/src/test/java/com/nuclearthinking/game/utils/PrinterTest.java
@@ -0,0 +1,10 @@
+package com.nuclearthinking.game.utils;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by Vladislav Radchenko on 22.12.2015.
+ */
+public class PrinterTest {
+
+}
