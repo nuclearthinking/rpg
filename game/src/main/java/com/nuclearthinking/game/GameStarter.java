ff81fba7ae394c6f9f8cf4fa075f825dad5a3a56
Izonami <kuokatsu@gmail.com>
2015-12-23 14:37:56 +0600

    Add Main Class

---
+++ game/src/main/java/com/nuclearthinking/game/GameStarter.java
@@ -0,0 +1,41 @@
+package com.nuclearthinking.game;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by kuksin-mv on 23.12.2015.
+ */
+public final class GameStarter
+{
+    private static final Logger LOG = LoggerFactory.getLogger(GameStarter.class);
+    private static final String LOG_FOLDER = "log";
+    private static final String LOG_NAME = "./log.cfg";
+
+    public static GameStarter gameStarter;
+
+    public GameStarter() throws Exception
+    {
+        printSection("Player load");
+        //TODO: Player.getInstance();
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        printSection("Config loading");
+        //TODO: Config.load(); //Конфиги вообще нужны ?
+
+        gameStarter = new GameStarter();
+    }
+
+    public static void printSection(String s)
+    {
+        s = "=[ " + s + " ]";
+        while (s.length() < 61)
+        {
+            s = "-" + s;
+        }
+        LOG.info(s);
+    }
+}
