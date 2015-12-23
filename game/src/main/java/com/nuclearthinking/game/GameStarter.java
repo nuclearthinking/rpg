2d6b1002b5ec6a75c2ea4aaef119611e47106710
Vladislav Radchenko <v.radchenko@madnet-team.ru>
2015-12-23 13:10:30 +0300

    player creation, setting name

--- game/src/main/java/com/nuclearthinking/game/GameStarter.java
+++ game/src/main/java/com/nuclearthinking/game/GameStarter.java
@@ -1,6 +1,8 @@
 package com.nuclearthinking.game;
 
 
+import com.nuclearthinking.game.obj.Player;
+import com.nuclearthinking.game.utils.UserInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,29 +14,37 @@ import org.slf4j.LoggerFactory;
  * @author kuksin-mv (onfient@gmail.com)
  */
 public final class GameStarter {
-    private static final Logger LOG = LoggerFactory.getLogger(GameStarter.class);
+//    private static final Logger LOG = LoggerFactory.getLogger(GameStarter.class);
     private static final String LOG_FOLDER = "log";
     private static final String LOG_NAME = "./log.cfg";
 
     public static GameStarter gameStarter;
+    private UserInput input = new UserInput();
 
     public GameStarter() throws Exception {
-        printSection("Player load");
-        //TODO: Player.getInstance();
+        // printSection("Player load");
+        Player player = Player.getInstance();
+
+        if (player.getName() == null) {
+            System.out.println("Введите имя персонажа");
+            player.setName(input.getUserInput());
+            System.out.println("Добро пожаловать " + player.getName());
+        }
+
     }
 
     public static void main(String[] args) throws Exception {
-        printSection("Config loading");
+//        printSection("Config loading");
         //TODO: Config.load(); //Конфиги вообще нужны ?
 
         gameStarter = new GameStarter();
     }
 
-    public static void printSection(String s) {
-        s = "=[ " + s + " ]";
-        while (s.length() < 61) {
-            s = "-" + s;
-        }
-        LOG.info(s);
-    }
+//    public static void printSection(String s) {
+//        s = "=[ " + s + " ]";
+//        while (s.length() < 61) {
+//            s = "-" + s;
+//        }
+//        LOG.info(s);
+//    }
 }
