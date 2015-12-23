259f27cdf587ffdc731846322084f97bb5ccb7d4
Vladislav Radchenko <onifent@gmail.com>
2015-12-23 08:00:46 +0300

    +tests for checkinput method

--- game/src/test/java/com/nuclearthinking/game/utils/WriterTest.java
+++ game/src/test/java/com/nuclearthinking/game/utils/WriterTest.java
@@ -34,4 +34,36 @@ public class WriterTest {
     public void checkInputTestSomeWhitespaces() {
         Assert.assertEquals(checkInput("  "),false);
     }
+
+    @Test(testName = "допсимволы")
+    public void checkInputTestNotLetterSymbols(){
+        Assert.assertEquals(checkInput("][}{!]"),false);
+    }
+
+    @Test(testName = "кирилический текст")
+    public void checkInputCyrilic(){
+        Assert.assertEquals(checkInput("фывфывфы"),true);
+    }
+
+    @Test(testName = "кирилически и английский текст")
+    public void checkInputMultitext(){
+        Assert.assertEquals(checkInput("фывqwe"),true);
+    }
+
+    @Test(testName = "текст и цифры")
+    public void checkInputTextAndNum(){
+        Assert.assertEquals(checkInput("1qwe3"),true);
+    }
+
+    @Test(testName = "длинный текст, слишком длинный")
+    public void checkInputLongString(){
+        Assert.assertEquals(checkInput("йцукенгшщзфывапр"),false);
+    }
+
+    @Test(testName = "длинный текст, в самый раз")
+    public void checkInputLongStringCorrect(){
+        Assert.assertEquals(checkInput("йцуфыв€чссмивап"),true);
+    }
+
+
 }
