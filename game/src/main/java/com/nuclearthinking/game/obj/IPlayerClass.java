1206fe54b43ee74ad9a867bc0db42efe9588c7b3
Vladislav Radchenko <v.radchenko@madnet-team.ru>
2015-12-25 13:12:30 +0300

    Добавил классы для персонажа

---
+++ game/src/main/java/com/nuclearthinking/game/obj/IPlayerClass.java
@@ -0,0 +1,21 @@
+package com.nuclearthinking.game.obj;
+
+/**
+ * Created
+ * Date: 25.12.2015
+ * Time: 13:07
+ *
+ * @author Vladislav Radchenko (onfient@gmail.com)
+ */
+public interface IPlayerClass {
+
+    int pDamage(Player player);
+
+    int mDamage(Player player);
+
+    void levelUp(Player player);
+
+    int mDefence(Player player);
+
+    int pDefence(Player player);
+}
---
+++ game/src/main/java/com/nuclearthinking/game/obj/WarriorClass.java
@@ -0,0 +1,35 @@
+package com.nuclearthinking.game.obj;
+
+/**
+ * Date: 25.12.2015
+ * Time: 13:06
+ *
+ * @author Vladislav Radchenko (onifent@gmail.com)
+ */
+
+public class WarriorClass extends PlayerClass implements IPlayerClass{
+    @Override
+    public int pDamage(Player player) {
+        return 0;
+    }
+
+    @Override
+    public int mDamage(Player player) {
+        return 0;
+    }
+
+    @Override
+    public void levelUp(Player player) {
+
+    }
+
+    @Override
+    public int mDefence(Player player) {
+        return 0;
+    }
+
+    @Override
+    public int pDefence(Player player) {
+        return 0;
+    }
+}
