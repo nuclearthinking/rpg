153ab52a8d6ff165156fcc87fa1c3f2505e2d524
Vladislav Radchenko <onifent@gmail.com>
2015-12-26 00:20:35 +0300

    ResourceUtil.getResourceAsFile добавил метод для получения ресурса в виде файла

--- game/src/main/java/com/nuclearthinking/game/utils/ResourceUtil.java
+++ game/src/main/java/com/nuclearthinking/game/utils/ResourceUtil.java
@@ -1,10 +1,14 @@
 package com.nuclearthinking.game.utils;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
  * Created by onifent
- *
  */
 
 public class ResourceUtil {
@@ -21,5 +25,24 @@ public class ResourceUtil {
             e.printStackTrace();
         }
         return is;
+    }
+
+    public File getResourceAsFile(String path) {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classLoader.getResourceAsStream(path);
+        File tempFile = null;
+        try {
+            tempFile = File.createTempFile("stream2file", ".tmp");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert tempFile != null;
+        tempFile.deleteOnExit();
+        try (FileOutputStream out = new FileOutputStream(tempFile)) {
+            IOUtils.copy(is, out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tempFile;
     }
 }
