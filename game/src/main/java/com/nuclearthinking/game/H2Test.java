065af567c6b111590e1a174ac4b0bb2a71907926
Vladislav Radchenko <v.radchenko@madnet-team.ru>
2016-01-14 15:45:58 +0300

    хэлоу ворд h2 db

---
+++ game/src/main/java/com/nuclearthinking/game/H2Test.java
@@ -0,0 +1,38 @@
+package com.nuclearthinking.game;
+
+import org.h2.tools.DeleteDbFiles;
+
+import java.sql.*;
+
+/**
+ * Date: 14.01.2016
+ * Time: 15:27
+ *
+ * @author Vladislav Radchenko (onifent@gmail.com)
+ */
+
+
+public class H2Test {
+    public static void main(String... args) throws Exception {
+        // delete the database named 'test' in the user home directory
+        DeleteDbFiles.execute("~", "test", true);
+
+        Class.forName("org.h2.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:h2:~/test");
+        Statement stat = conn.createStatement();
+
+        // this line would initialize the database
+        // from the SQL script file 'init.sql'
+        // stat.execute("runscript from 'init.sql'");
+
+        stat.execute("create table test(id int primary key, name varchar(255))");
+        stat.execute("insert into test values(1, 'Hello')");
+        ResultSet rs;
+        rs = stat.executeQuery("select * from test");
+        while (rs.next()) {
+            System.out.println(rs.getString("name"));
+        }
+        stat.close();
+        conn.close();
+    }
+}
