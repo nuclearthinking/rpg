d474bf60934b9e2422a3c32c1fff6d1af52b75a9
Izonami <kuokatsu@gmail.com>
2016-01-25 23:22:36 +0600

    App Start

---
+++ game/src/main/java/com/izonami/app/Controller.java
@@ -0,0 +1,18 @@
+package com.izonami.app;/**
+ * Created by Izonami on 25.01.2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Controller extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
