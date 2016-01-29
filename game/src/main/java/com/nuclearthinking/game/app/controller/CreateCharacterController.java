33ff1e9a1606f2d166aa898f4e1169851271a583
Izonami <kuokatsu@gmail.com>
2016-01-29 12:14:55 +0600

    Немного переводов для создания персонажа

--- game/src/main/java/com/nuclearthinking/game/app/controller/CreateCharacterController.java
+++ game/src/main/java/com/nuclearthinking/game/app/controller/CreateCharacterController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Tooltip;
 
 /**
  * Created by kuksin-mv on 28.01.2016.
@@ -24,9 +23,9 @@ public class CreateCharacterController
     @FXML
     private void initialize()
     {
-        playerName.setTooltip(new Tooltip("Fill Player Name"));
+        //playerName.setTooltip(new Tooltip("Fill Player Name"));
 
-        selectClass.setTooltip(new Tooltip("Select the Class"));
+        //selectClass.setTooltip(new Tooltip("Select the Class"));
         selectClass.getItems().addAll("Mag", "Warrior", "Rogue");
         selectClass.getSelectionModel().selectFirst();
 
